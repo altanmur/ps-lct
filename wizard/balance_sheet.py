037e45d7658c35da87551a0e2c42835e62ccd358
tgroutars <tgroutars@gmail.com>
2014-05-02 14:12:05 +0200

    Modification of balance sheet

--- wizard/balance_sheet.py
+++ wizard/balance_sheet.py
@@ -34,30 +34,33 @@ class balance_sheet(osv.osv_memory):
     _name = "balance.sheet"
     
     
-    def _get_root_parents(self, cr, uid, ids, parents, children, context=None):
-       
-        if children == []:
-            return parents
-        parent = children[0].parent_id
-        if not parent:
-            if children[0] not in parents:
-                parents.append(children[0])
-            del children[0]
-        else :
-            children[0] = parent
-        return self._get_root_parents(cr, uid, ids, parents, children, context=context)
+    
+    #~ def _get_root_parents(self, cr, uid, ids, parents, children, context=None):
+       #~ 
+        #~ if children == []:
+            #~ return parents
+        #~ parent = children[0].parent_id
+        #~ if not parent:
+            #~ if children[0] not in parents:
+                #~ parents.append(children[0])
+            #~ del children[0]
+        #~ else :
+            #~ children[0] = parent
+        #~ return self._get_root_parents(cr, uid, ids, parents, children, context=context)
         
     def _format_sheet(self, cr, uid, ids, sheet, context=None):
-        for i in range(1,4):
-            col=sheet.col(i)
-            col.width = 7000
+        
+        col=sheet.col(1)
+        sheet.col(1).width = 5000
+        sheet.col(2).width = 12000
+        sheet.col(3).width = 5000
             
         row = sheet.row(3)
         row.height_mismatch = True
         row.height = 20*30
         sheet.write_merge(3,3,1,3,"Balance Sheet",easyxf(
             'font: name Calibri, height 320,bold on;'
-            'borders: left thick,right thick,top thick;'
+            'borders: left thick, right thick, top thick;'
             'align: horizontal center, vertical center;'
             ))
         row = sheet.row(4)
@@ -99,67 +102,145 @@ class balance_sheet(osv.osv_memory):
             'border: right thin,bottom thin;'
             ))
             
-    def _get_accounts(self, cr, uid,  ids, accounts, context=None):
-        result_accounts = []
-        while True :
-            if accounts == []:
-                break
-            if accounts[0].child_parent_ids != []:
-                for child in accounts[0].child_parent_ids:
-                    accounts.insert(1,child)
-            result_accounts.append(accounts[0])
-            accounts.pop(0)
-        return result_accounts
+
         
         
     def _fill_data(self, cr, uid,  ids, sheet, context=None):
-
-        i = 10
-        accounts = []
-        for journal in self.browse(cr,uid,ids,context=context)[0].journal_ids :
-            account = journal.default_debit_account_id
-            if account:
-                if account not in accounts:
-                    accounts.append(account)
         
-        parent_accounts = self._get_root_parents(cr, uid, ids, [], accounts, context=context)
-        accounts = self._get_accounts(cr, uid,  ids, parent_accounts, context=context)
-        accounts.reverse()
-      
-        for i in range(0,len(accounts)):
-            row = sheet.row(i+10)
-            if accounts[i].child_parent_ids == [] :
+        
+        if self.browse(cr,uid,ids,context=context)[0].date_from : context['date_from'] = self.browse(cr,uid,ids,context=context)[0].date_from
+        if self.browse(cr,uid,ids,context=context)[0].date_to : context['date_to'] = self.browse(cr,uid,ids,context=context)[0].date_to
+        if self.browse(cr,uid,ids,context=context)[0].period_from : context['date_from'] = self.browse(cr,uid,ids,context=context)[0].period_from.date_start
+        if self.browse(cr,uid,ids,context=context)[0].period_to : context['date_to'] = self.browse(cr,uid,ids,context=context)[0].period_to.date_stop
+        #self.write(cr, uid, ids,{ 'chart_account_id': self.pool.get('account.account').search(cr,uid,[('name','ilike','IFRS')],context=context)[0]})
+        
+        
+        domain = [('note','ilike','IFRS'),'|',('code','ilike','2%'),('code','ilike','3%')]
+        acc_ids = self.pool.get('account.account').search(cr, uid,domain,order="code")
+        import pdb; pdb.set_trace()
+        accounts = self.pool.get('account.account').browse(cr,uid,acc_ids,context=context)
+        
+        
+        
+        if accounts == []:
+            return 
+        
+        row = sheet.row(10)
+        if accounts[0].code[1] == "X":
+            row.write(1,accounts[0].code,easyxf(
+                'font: bold on, height 240, colour white;'
+                'pattern: pattern solid, fore_colour black;'
+                'borders: left thin,right thin,bottom thin, top thin;'
+                'align: horizontal center, vertical center;'
+                ))
+            row.write(2,accounts[0].name,easyxf(
+                'font: bold on, height 240, colour white;'
+                'pattern: pattern solid, fore_colour black;'
+                'borders: left thin,right thin,bottom thin, top thin;'
+                'align: horizontal center, vertical center;'
+                ))
+            row.write(3,accounts[0].debit - accounts[0].debit,easyxf(
+                'font:bold on, height 240, colour white;'
+                'pattern: pattern solid, fore_colour black;'
+                'borders: left thin,right thin,bottom thin, top thin;'
+                'align: horizontal center, vertical center;'
+                ))
+        elif accounts[0].child_parent_ids:
+            row.write(1,accounts[0].code,easyxf(
+                'font: bold on, height 240;'
+                'borders: left thin,right thin,bottom thin, top thin;'
+                'align: horizontal center, vertical center;'
+                ))
+            row.write(2,accounts[0].name,easyxf(
+                'font: bold on, height 240;'
+                'borders: left thin,right thin,bottom thin, top thin;'
+                'align: horizontal center, vertical center;'
+                ))
+            row.write(3,accounts[0].debit - accounts[0].debit,easyxf(
+                'font:bold on, height 240, colour white;'
+                'pattern: pattern solid, fore_colour grey25;'
+                'borders: left thin,right thin,bottom thin, top thin;'
+                'align: horizontal center, vertical center;'
+                ))
+        elif accounts[0].code[-1] != "X":
+            row.write(1,accounts[0].code,easyxf(
+                'align: horizontal center, vertical center;'
+                'border: left thin'
+                ))
+            row.write(2,accounts[0].name,easyxf(
+                'align: horizontal center, vertical center;'
+                'border: left thin, right thin'
+                ))
+            row.write(3,accounts[0].debit - accounts[0].debit,easyxf(
+                'align: horizontal center, vertical center;'
+                'border: right thin'
+                ))
+        j = 0
+        for i in range(1,len(accounts)):
+            j = j+1
+            row = sheet.row(j+10)
+            if accounts[i].code[1] == "X":
                 row.write(1,accounts[i].code,easyxf(
+                    'font: bold on, height 240, colour white;'
+                    'pattern: pattern solid, fore_colour black;'
+                    'borders: left thin,right thin,bottom thin, top thin;'
                     'align: horizontal center, vertical center;'
                     ))
                 row.write(2,accounts[i].name,easyxf(
+                    'font: bold on, height 240, colour white;'
+                    'pattern: pattern solid, fore_colour black;'
+                    'borders: left thin,right thin,bottom thin, top thin;'
                     'align: horizontal center, vertical center;'
                     ))
-                row.write(3,accounts[i].debit - accounts[i].debit,easyxf(
-                    'align: horizontal center, vertical center;'
+                row.set_cell_number(3,accounts[i].balance,easyxf(
+                    'font:bold on, height 240, colour white;'
+                    'pattern: pattern solid, fore_colour black;'
+                    'borders: left thin,right thin,bottom thin, top thin;'
+                    'align: horizontal center, vertical center;',
+                    num_format_str='#,##0'
                     ))
-            else :
+            elif accounts[i].child_parent_ids != []:
                 row.write(1,accounts[i].code,easyxf(
-                    'font: bold on;'
+                    'font: bold on, height 240;'
                     'borders: left thin,right thin,bottom thin, top thin;'
                     'align: horizontal center, vertical center;'
                     ))
                 row.write(2,accounts[i].name,easyxf(
-                    'font: bold on;'
+                    'font: bold on, height 240;'
                     'borders: left thin,right thin,bottom thin, top thin;'
                     'align: horizontal center, vertical center;'
                     ))
-                row.write(3,accounts[i].debit - accounts[i].debit,easyxf(
-                    'font:bold on;'
+                row.set_cell_number(3,accounts[i].balance,easyxf(
+                    'font:bold on, height 240, colour white;'
+                    'pattern: pattern solid, fore_colour grey25;'
                     'borders: left thin,right thin,bottom thin, top thin;'
-                    'align: horizontal center, vertical center;'
+                    'align: horizontal center, vertical center;',
+                    num_format_str='#,##0'
                     ))
+            elif accounts[i].code[-1] != "X":
+                row.write(1,accounts[i].code,easyxf(
+                    'align: horizontal center, vertical center;'
+                    'border: left thin'
+                    ))
+                row.write(2,accounts[i].name,easyxf(
+                    'align: horizontal center, vertical center;'
+                    'border: left thin, right thin'
+                    ))
+                row.set_cell_number(3,accounts[i].balance,easyxf(
+                    'align: horizontal center, vertical center;'
+                    'border: right thin',
+                    num_format_str='#,##0'
+                    ))
+            else : j = j-1
+            
+                    
+        
+        
           
     def _write_report(self, cr, uid, ids, context=None):
-        report = Workbook()
+        report = Workbook(style_compression=2)
         sheet = report.add_sheet('Balance Sheet',cell_overwrite_ok=True) 
         self._format_sheet(cr, uid, ids, sheet=sheet,context=context)
-        
         self._fill_data( cr, uid, ids, sheet=sheet, context=context)
         return report
         
@@ -167,10 +248,11 @@ class balance_sheet(osv.osv_memory):
     def print_report(self, cr, uid, ids, name, context=None):
         if context is None:
             context = {}
-        print name
-
-        report = self._write_report(cr,uid,ids,context=context)
         
+        
+        #if self.browse(cr,uid,ids,context=context)[0].filter == "filter_no" :
+        report = self._write_report(cr,uid,ids,context=context)
+        #rt.add_sheet('Balance Sheet',cell_overwrite_ok=True)
         
         f = StringIO.StringIO()
         report.save(f)
