36342e73ab4d176a65853d9000ec1a6c6a681e65
Thomas Groutars <tgr@odoo.com>
2014-09-02 10:30:08 +0200

    [FIX] Write to StringIO instead of temp file while exporting

--- lct_tos_integration/models/ftp_config.py
+++ lct_tos_integration/models/ftp_config.py
@@ -27,7 +27,6 @@ from StringIO import StringIO
 from datetime import datetime
 import io
 import traceback
-from tempfile import NamedTemporaryFile
 
 
 class ftp_config(osv.osv):
@@ -164,7 +163,7 @@ class ftp_config(osv.osv):
         invoice = invoice_model.browse(cr, uid, app_id, context=context)
         voucher = voucher_model.browse(cr, uid, payment_id, context=context)
         values = {
-            'customer_id': invoice.partner_id.name,
+            'customer_id': invoice.partner_id.id,
             'individual_customer': 'IND' if invoice.individual_cust else 'STD',
             'appointment_reference': invoice.appoint_ref,
             'appointment_date': invoice.appoint_date,
@@ -176,21 +175,22 @@ class ftp_config(osv.osv):
         self._dict_to_tree(values, ET.SubElement(root, 'appointment'))
         return root
 
-    def _write_xml_file(self, root):
-        f = NamedTemporaryFile("w+")
+    def _xml_to_file(self, root):
+        f = StringIO()
         f.write(u'<?xml version="1.0" encoding="utf-8"?>')
         f.write(ET.tostring(root, encoding='utf-8', pretty_print=True).decode('utf-8'))
         return f
 
-    def _upload_file(self, cr, uid, temp_file, file_name, context=None):
+    def _upload_file(self, cr, uid, f, file_name, context=None):
+        import ipdb; ipdb.set_trace()
         ftp_config_ids = self.search(cr, uid, [('active','=',True)], context=context)
         ftp_config_id = ftp_config_ids and ftp_config_ids[0] or False
         config_obj = self.browse(cr, uid, ftp_config_id, context=context)
         ftp = FTP(host=config_obj.addr, user=config_obj.user, passwd=config_obj.psswd)
         inbound_path =  config_obj.inbound_path.rstrip(os.sep) + os.sep
         ftp.cwd(inbound_path)
-        temp_file.seek(0)
-        ftp.storlines('STOR ' + file_name, temp_file)
+        f.seek(0)
+        ftp.storlines('STOR ' + file_name, f)
 
     def export_partners(self, cr, uid, partner_ids, context=None):
         if not partner_ids:
@@ -202,9 +202,9 @@ class ftp_config(osv.osv):
 
         sequence = self.pool.get('ir.sequence').get_next_by_xml_id(cr, uid, 'lct_tos_integration', 'sequence_partner_export', context=context)
 
-        local_path = __file__.split('models')[0] + "tmp/"
         file_name = 'CUS_' + datetime.today().strftime('%y%m%d') + '_' + sequence + '.xml'
-        self._upload_file(cr, uid, self._write_xml_file(root), file_name, context=context)
+        f = self._xml_to_file(root)
+        self._upload_file(cr, uid, f, file_name, context=context)
         return partner_ids
 
     def export_app(self, cr, uid, app_id, payment_id, context=None):
@@ -212,6 +212,6 @@ class ftp_config(osv.osv):
             return []
         root = self._write_app_tree(cr, uid, app_id, payment_id, context=context)
         sequence = self.pool.get('ir.sequence').get_next_by_xml_id(cr, uid, 'lct_tos_integration', 'sequence_appointment_validate_export', context=context)
-        local_path = __file__.split('models')[0] + "tmp/"
         file_name = 'APP_' + datetime.today().strftime('%y%m%d') + '_' + sequence + '.xml'
-        self._upload_file(cr, uid, self._write_xml_file(root), file_name, context=context)
+        f = self._xml_to_file(root)
+        self._upload_file(cr, uid, f, file_name, context=context)
