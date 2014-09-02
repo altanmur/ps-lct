adca7b01a473b7e4443abc29ef5a05fc44d2f467
Thomas Groutars <tgr@odoo.com>
2014-09-02 09:53:21 +0200

    [IMP] Change customer id in a StringIO instead of writing directly on file + Delete only draft invoices

--- lct_tos_integration/tests/test_import.py
+++ lct_tos_integration/tests/test_import.py
@@ -3,6 +3,7 @@ from openerp.osv import osv
 import os
 from ftplib import FTP
 from lxml import etree as ET
+from StringIO import StringIO
 
 
 class TestImport(TransactionCase):
@@ -164,11 +165,13 @@ class TestImport(TransactionCase):
     def _prepare_import(self):
         cr, uid = self.cr, self.uid
         config = self.config
+
         self._create_pricelist()
         self.partner_id = self.partner_model.create(cr, uid,{
             'name': 'Test Customer',
             'property_product_pricelist': self.pricelist.id,
             })
+
         ftp = FTP(host=config['addr'], user=config['user'], passwd=config['psswd'])
         ftp.cwd(config['outbound_path'])
         for outbound_file in ftp.nlst():
@@ -177,37 +180,37 @@ class TestImport(TransactionCase):
                     ftp.delete(outbound_file)
                 except:
                     ftp.rmd(outbound_file)
-        xml_dirs = ['APP_XML_files', 'VBL_XML_files']
+
+        xml_files = []
         local_path_path = os.path.join(__file__.split(__file__.split(os.sep)[-1])[0], 'xml_files')
-        config = self.config
-        ftp = FTP(host=config['addr'], user=config['user'], passwd=config['psswd'])
-        ftp.cwd(config['outbound_path'])
-        for outbound_file in ftp.nlst():
-            if outbound_file != "done":
-                try:
-                    ftp.delete(outbound_file)
-                except:
-                    ftp.rmd(outbound_file)
-        local_path_path = os.path.join(__file__.split(__file__.split(os.sep)[-1])[0], 'xml_files')
+
         app_dir = os.path.join(local_path_path, 'APP_XML_files')
-        for xml_file in (os.path.join(app_dir, file_name) for file_name in  os.listdir(app_dir)):
+        app_files = [os.path.join(app_dir, file_name) for file_name in  os.listdir(app_dir)]
+        for xml_file in app_files:
             tree = ET.parse(open(xml_file))
             root = tree.getroot()
             for appoint in root.findall('appointment'):
                 appoint.find('customer_id').text = str(self.partner_id)
-            open(xml_file, 'w+').write(ET.tostring(tree, pretty_print=True))
+            file_name = xml_file.split(os.sep)[-1]
+            f = StringIO()
+            f.write(ET.tostring(tree, pretty_print=True))
+            xml_files.append((file_name, f))
+
         vbl_dir = os.path.join(local_path_path, 'VBL_XML_files')
-        for xml_file in (os.path.join(vbl_dir, file_name) for file_name in  os.listdir(vbl_dir)):
+        vbl_files = [os.path.join(vbl_dir, file_name) for file_name in  os.listdir(vbl_dir)]
+        for xml_file in vbl_files:
             tree = ET.parse(open(xml_file))
             root = tree.getroot()
             for vessel in root.findall('vbilling'):
                 vessel.find('vessel_operator_id').text = str(self.partner_id)
-            open(xml_file, 'w+').write(ET.tostring(tree, pretty_print=True))
-        xml_dirs = [app_dir, vbl_dir]
-        for xml_dir in xml_dirs:
-            for xml_file in os.listdir(xml_dir):
-                xml_abs_file = os.path.join(xml_dir, xml_file)
-                ftp.storlines(''.join(['STOR ', xml_file]), open(xml_abs_file))
+            file_name = xml_file.split(os.sep)[-1]
+            f = StringIO()
+            f.write(ET.tostring(tree, pretty_print=True))
+            xml_files.append((file_name, f))
+
+        for xml_file in xml_files:
+            xml_file[1].seek(0)
+            ftp.storlines(''.join(['STOR ', xml_file[0]]), xml_file[1])
 
     def test_import(self):
         cr, uid = self.cr, self.uid
@@ -215,7 +218,7 @@ class TestImport(TransactionCase):
         invoice_model, import_data_model = self.invoice_model, self.import_data_model
         product_model = self.registry('product.product')
         self._prepare_import()
-        inv_ids = invoice_model.search(cr, uid, ['|',('type2','=','vessel'),('type2','=','appointment')])
+        inv_ids = invoice_model.search(cr, uid, [('state','=','draft'),'|',('type2','=','vessel'),('type2','=','appointment')])
         if inv_ids:
             invoice_model.unlink(cr, uid, inv_ids)
         data_ids = import_data_model.search(cr, uid, [])
@@ -223,7 +226,7 @@ class TestImport(TransactionCase):
             import_data_model.unlink(cr, uid, data_ids)
         ftp_config_model.button_import_ftp_data(cr, uid, [self.config_id])
 
-        appoints = invoice_model.browse(cr, uid, invoice_model.search(cr, uid, [('type2','=','appointment')], order='appoint_ref'))
+        appoints = invoice_model.browse(cr, uid, invoice_model.search(cr, uid, [('state','=','draft'),('type2','=','appointment')], order='appoint_ref'))
         self.assertTrue(len(appoints) == 2, 'Importing should create 2 appointments')
         self.assertTrue(appoints[0].appoint_ref == 'LCT2014062400289', 'Importing should create an appointment with reference: LCT2014062400289')
         lines = sorted(appoints[0].invoice_line, key=lambda x: x.name.lower())
@@ -240,7 +243,7 @@ class TestImport(TransactionCase):
         self.assertTrue(appoints[1].appoint_ref == 'LCT2014070900019', 'Importing should create an appointment with reference: LCT2014070900019')
         self.assertTrue(len(appoints[1].invoice_line) == 0)
 
-        vessels = invoice_model.browse(cr, uid, invoice_model.search(cr, uid, [('type2','=','vessel')], order='call_sign'))
+        vessels = invoice_model.browse(cr, uid, invoice_model.search(cr, uid, [('state','=','draft'),('type2','=','vessel')], order='call_sign'))
         self.assertTrue(len(vessels) == 1, 'Importing should create 1 vessel billing')
         self.assertTrue(vessels[0].call_sign == 'CALLSIGN000', 'Importing should create an appointment with reference: CALLSIGN000')
 
