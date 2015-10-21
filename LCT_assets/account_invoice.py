61610f609060c98667722b0c30a9e51719d7469b
Stan <sts@odoo.com>
2015-10-21 16:46:01 +0200

    [DEV] copy 1st depreciation date and invoice date on asset on invoice validation

---
+++ LCT_assets/account_invoice.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, osv
+from datetime import datetime, date
+
+class account_invoice_line(osv.osv):
+
+    _inherit = "account.invoice.line"
+
+    _columns = {
+        'first_depreciation_date': fields.date('First Depreciation Date'),
+    }
+    
+    def asset_create(self, cr, uid, lines, context=None):
+        context = context or {}
+        asset_obj = self.pool.get('account.asset.asset')
+        for line in lines:
+            if line.asset_category_id:
+                vals = {
+                    'name': line.name,
+                    'code': line.invoice_id.number or False,
+                    'category_id': line.asset_category_id.id,
+                    'purchase_value': line.price_subtotal,
+                    'period_id': line.invoice_id.period_id.id,
+                    'partner_id': line.invoice_id.partner_id.id,
+                    'company_id': line.invoice_id.company_id.id,
+                    'currency_id': line.invoice_id.currency_id.id,
+                    'purchase_date' : line.first_depreciation_date if line.first_depreciation_date else line.invoice_id.date_invoice,
+                    'purchase_date_2': line.invoice_id.date_invoice,
+                }
+                changed_vals = asset_obj.onchange_category_id(cr, uid, [], vals['category_id'], context=context)
+                vals.update(changed_vals['value'])
+                asset_id = asset_obj.create(cr, uid, vals, context=context)
+                if line.asset_category_id.open_asset:
+                    asset_obj.validate(cr, uid, [asset_id], context=context)
+        return True
